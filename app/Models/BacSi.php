199a34554b2e8fb2a4058ad6d9035e41aff16768
Thao <nguyenthithuthao452003@gmail.com>
2025-04-05 19:06:42 +0700

    thao

--- app/Models/BacSi.php
+++ app/Models/BacSi.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class BacSi extends Model
 {
-    //
+    protected $table = 'bac_sis';
+    protected $fillable = [
+        'ten_bs',
+        'gioi_tinh',
+        'email',
+        'password',
+        'mo_ta',
+        'hinh_anh',
+        'tinh_trang',
+        'id_chucvu',
+        'chuyen_khoa',
+        'tien_kham',
+    ];
 }
--- app/Models/pet.php
+++ app/Models/pet.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class pet extends Model
 {
-    //
+    protected $table = 'pets';
+    protected $fillable = [
+        'id_kh',
+        'ten_pet',
+        'chung_loai',
+        'gioi_tinh',
+        'tuoi',
+        'hinh_anh',
+        'can_nang',
+        'tinh_trang',
+    ];
 }
