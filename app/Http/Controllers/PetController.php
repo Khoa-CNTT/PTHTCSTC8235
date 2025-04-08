eeb4a170667e97dd5524d071a98c6ee6cc319fd4
doancarat <aligyokugaming@gmail.com>
2025-04-08 16:42:12 +0700

    Doan

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pet;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+    public function Load()
+    {
+        $data = pet::get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+    public function Doitt(Request $request)
+    {
+        $data = pet::find($request->id);
+        if ($data->tinh_trang == 1) {
+            $data->tinh_trang = 0;
+            $data->save();
+        } else {
+            $data->tinh_trang = 1;
+            $data->save();
+        }
+        return response()->json([
+            'status' => 1,
+            'message' => 'Sửa đổi trạng thái thành công'
+        ]);
+    }
+    public function Xoa(Request $request)
+    {
+        pet::where('id', $request->id)->delete();
+        return response()->json([
+            'status' => 1,
+            'message' => 'Xoá pet thành công'
+        ]);
+    }
+    public function Sua(Request $request)
+    {
+        pet::find($request->id)->update($request->all());
+        return response()->json([
+            'status' => 1,
+            'message' => 'Sửa thông tin pet thành công'
+        ]);
+    }
+}
--- database/migrations/2025_04_05_101259_create_luongs_table.php
+++ database/migrations/2025_04_05_101259_create_luongs_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('luongs', function (Blueprint $table) {
+            
             $table->id();
             $table->string('ten_kho');
             $table->string('dia_chi');
