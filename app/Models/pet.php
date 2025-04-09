d0b5d4af5091a74c64d22891b5b0088ef202482c
doancarat <aligyokugaming@gmail.com>
2025-04-09 16:29:44 +0700

    doan

--- app/Models/pet.php
+++ app/Models/pet.php
@@ -17,15 +17,4 @@ class pet extends Model
         'can_nang',
         'tinh_trang',
     ];
-    protected $table = 'pets';
-    protected $fillable = [
-        'id_kh',
-        'ten_pet',
-        'chung_loai',
-        'gioi_tinh',
-        'tuoi',
-        'hinh_anh',
-        'can_nang',
-        'tinh_trang',
-    ];
 }
