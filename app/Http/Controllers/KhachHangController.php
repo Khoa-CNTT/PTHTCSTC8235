bf41bd26dc0808240ee3630a6b331e2462cf6432
suyg3tsu <145313056+suyg3tsu@users.noreply.github.com>
2025-04-11 02:51:05 +0700

    dangNhap

--- app/Http/Controllers/KhachHangController.php
+++ app/Http/Controllers/KhachHangController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\KhachHang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class KhachHangController extends Controller
 {
@@ -33,10 +35,65 @@ class KhachHangController extends Controller
         $khach_hang->save();
         $data['ho_va_ten'] = $khach_hang->ho_va_ten;
         $data['link'] = "http://localhost:5173/khach-hang/" . $khach_hang->hash_active;
-        Mail::to($request->email)->send(new \App\Mail\MuaMail('Kich Hoat Tai khoan', 'ViewMail', $data));
+        Mail::to($request->email)->send(new \App\Mail\TaiKhoan('Kich Hoat Tai khoan', 'GiaoDienMail', $data));
         return response()->json([
             "status" => "1",
             "message" => "them thanh cong"
         ]);
     }
+    public function sendMail(Request $request){
+        $hash_active = Str::uuid();
+        $khach_hang = KhachHang::where('email',$request->email)->first();
+        $khach_hang['hash_active']=$hash_active;
+        $khach_hang->save();
+        $data['ho_va_ten'] = $khach_hang->ho_va_ten;
+        $data['link'] = "http://localhost:5173/doi-mat-khau/" . $khach_hang->hash_active;
+        Mail::to($request->email)->send(new \App\Mail\TaiKhoan('Kich Hoat Tai khoan', 'DoiMatKhauMail', $data));
+        return response()->json([
+            "message" => "Kiểm tra email của bạn"
+        ]);
+    }
+    public function doimk(Request $request){
+        $khach_hang = KhachHang::where('hash_active', $request->ma)
+        ->first();
+        if($khach_hang){
+            $khach_hang['password']=bcrypt($request->pass);
+            $khach_hang['hash_active']='';
+            $khach_hang->save();
+            return response()->json([
+                "status" => 1,
+                "message" => "Đổi mật khẩu thành công "
+            ]);
+        }else{
+            return response()->json([
+                "status" => 2,
+                "message" => "Đổi mật khẩu không thành công "
+            ]);
+        }
+    }
+    public function kichHoat(Request $request){
+        $check = KhachHang::where('hash_active', $request->id_khach_hang)
+                        ->first();
+        if($check){
+            if($check->is_active==0){
+                $check->is_active=1;
+                $check->save();
+                return response()->json([
+                    "status" => 1,
+                    "message" => "kich hoat thanh cong"
+                ]);
+            }else {
+                return response()->json([
+                    "status" => 2,
+                    "message" => "tai khoan da dc kich hoat truoc do"
+                ]);
+            }
+        }else{
+            return response()->json([
+                "status" => 0,
+                "message" => "kich hoat khong thanh cong"
+            ]);
+        }
+    }
+
 }
--- app/Http/Middleware/KiemTraMiddleware.php
+++ app/Http/Middleware/KiemTraMiddleware.php
@@ -4,19 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class KiemTraMiddleware
 {
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next): Response
     {
-        $user = Auth::guard('sanctum')->user();
-        if ($user && $user instanceof \App\Models\NhanVien) {
-            return $next($request);
-        } else {
-            return response()->json('bien');
-        }
+        return $next($request);
     }
 }
